aaa1ca9456b8102cab5428cfbdc33ec079afafc6
robinhlgrsn <robinholgersson91@gmail.com>
2021-09-07 11:14:59 +0200

    initial setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div>
+      <h1 className="container mx-auto text-2xl md:text-4xl lg:text-6xl shadow-2xl">Sockets</h1>
+    </div>
+  );
+}
+
+export default App;
