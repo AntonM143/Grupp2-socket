5accf9b164f1f0c7e09ac3da2da7e19d3d491c12
robinhlgrsn <robinholgersson91@gmail.com>
2021-09-10 16:52:56 +0200

    rooms start.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { io } from "socket.io-client";
 import Layout from "./components/Layout";
 import ChatInput from "./components/chat/ChatInput";
@@ -7,48 +7,40 @@ import StartModal from "./components/StartModal";
 import { v4 as uuid } from "uuid";
 import date from "./components/handlers/date";
 
+const socket = io("http://localhost:8000");
 
 function App() {
-
- 
+  const [rooms, setRooms] = useState([]);
   const [isTyping, setIsTyping] = useState("")
-  const [socket, setSocket] = useState('')
   const [enteredMessage, setEnteredMessage] = useState("");
-
   const [chatMessage, setChatMessage] = useState([]);
   const [startModal, setStartModal] = useState(true);
   const [user, setUser] = useState(null);
-  console.log(user, "👍")
+  const [currentRoom, setCurrentRoom] = useState('defaultRoom');
+  const roomNameRef = useRef()
+  const passwordRef = useRef()
 
-
+  console.log(currentRoom)
   useEffect(() => {
-    const socket = io("http://localhost:8000");
-    setSocket(socket)
-    
-   
-
-    return () => {
-
-      socket.off('message')
-    }
-  },[]);
-
-  
-  if(socket){
     socket.on("message", (message) => {
-      
-      setChatMessage([...chatMessage, message])
+      setChatMessage((prevState) => [...prevState, message]);
     })
+  
     socket.on("isTyping", (message) =>{
       setIsTyping(message)
     })
-  }
+    return () => {
+      socket.off('message')
+    }
+  }, []);
+  
+
   const enteredMessageHandler = (currentValue) =>{
     setEnteredMessage(currentValue)
     socket.emit("isTyping", {name: user.username, isTyping: true})
     }
     const sendMessage = () =>{
-      socket.emit("message", {
+      socket.emit('message', {
         ...user,
         id: uuid(),
         sendDate: date(),
@@ -73,10 +65,41 @@ function App() {
         setEnteredMessage("")
         socket.emit("isTyping", {isTyping: false})
     }
+
+    const roomHandler = (name) => {
+      socket.emit('leave', currentRoom);
+      socket.emit('join', name);
+      setCurrentRoom(name)
+      setChatMessage([])
+    }
+
+  
+    const createRoomHandler = (e) => {
+      e.preventDefault()
+      setRooms((prevState) => [...prevState, {
+        id: rooms.length + 1,
+        name: roomNameRef.current.value,
+      }])
+    }
+
+
+
   return (
     <Layout>
       {startModal ? <StartModal onUsernameHandler={usernameHandler} /> : 
         <>
+          <div className="p-10 text-gray-50">
+            <form onSubmit={createRoomHandler} className="flex flex-col text-gray-600">
+              <input placeholder="namn" type="text" ref={roomNameRef} />
+              <input placeholder="password" type="text" ref={passwordRef} />
+              <button>+</button>
+            </form>
+            {rooms.map((room) => (
+            <ul key={room.id}>
+              <li onClick={() => roomHandler(room.name)}>{room.name}</li>
+            </ul>
+          ))}
+          </div>
           <ChatList messageData={chatMessage} />
           <ChatInput onEnteredMessageHandler={enteredMessageHandler} enteredMessage={enteredMessage} onSendMessage={sendMessage} onIsTyping={isTyping} onSendItem={sendItem}/>
         </>
